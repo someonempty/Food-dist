19d3a6279c695712e63ff6e13be8518b9aab4e1a
someonempty <someonempty@gmail.com>
2022-08-09 22:03:00 +0300

    Добавляет модальное окно

--- js/script.js
+++ js/script.js
@@ -43,14 +43,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
         // Timer
 
-        const deadline = '2022-08-11';
+        const deadline = '2021-08-11';
 
         function getTimeRemaining(endtime) {
-            const t = Date.parse(endtime) - Date.parse(new Date()),
-                  days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            let days, hours, minutes, seconds;
+            const t = Date.parse(endtime) - Date.parse(new Date());
+
+            if(t <= 0) {
+                days = 0;
+                hours = 0;
+                minutes = 0;
+                seconds = 0;
+            }
+
+            else {
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
                   hours = Math.floor((t / (1000 * 60 * 60) % 24)),
                   minutes = Math.floor((t / 1000 / 60) % 60),
                   seconds = Math.floor((t / 1000) % 60);
+            }
 
                   return {
                     'total': t,
@@ -94,4 +105,39 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         setClock('.timer', deadline);
+
+
+        // Modal
+
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+              modal = document.querySelector('.modal'),
+              modalCloseBtn = document.querySelector('[data-close]');
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+                });
+
+        });     
+
+        function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+        
+        
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            closeModal();
+        })
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+        })
 });
